e4c54575d7dbb498b69bc9aa593b90d58b8f9123
Priscilla Souza <priscillasilva-24@hotmail.com>
2020-04-27 21:18:18 -0300

    define aleatoriedade da frutinha

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen"; //cor do quadrado
-    context.fillRect(0, 0, 16 * box, 16 * box); //onde vai acontece o jogo
+    context.fillStyle = "lightgreen"; 
+    context.fillRect(0, 0, 16 * box, 16 * box); 
 }
 
 function createSnake() {
@@ -21,9 +25,14 @@ function createSnake() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
-function update (event){
+function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left"; 
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
@@ -31,7 +40,6 @@ function update (event){
     
 }
 
-//função que vai parar iniciar 
 function iniciarJogo() {
 
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -41,6 +49,7 @@ function iniciarJogo() {
     
     criarBG();
     createSnake();
+    drawFood();
     
 
     let snakeX = snake[0].x;
@@ -51,7 +60,14 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
+
 
     let newHead = {
         x: snakeX,
